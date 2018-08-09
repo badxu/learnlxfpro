c9b43bfa52a50723d444c78040a7048d719c6b0e
badxu <35223644@qq.com>
2018-08-09 14:01:56 +0800

    some samples

---
+++ www/static/app.py
@@ -0,0 +1,24 @@
+import logging; logging.basicConfig(level=logging.INFO)
+
+import asyncio, os, json, time
+from datetime import datetime
+
+from aiohttp import web
+
+def index(request):
+    return web.Response(body=b'<h1>Awesome</h1>')
+
+@asyncio.coroutine
+async def init(event_loop):
+    #创建web服务器实例
+    app = web.Application(loop=event_loop)
+    app.router.add_route('GET', '/', index)
+    #利用event_loop.creat_server()创建TCP服务
+    app_runner = web.AppRunner(app)
+    srv = await event_loop.create_server(app_runner.app.make_handler(), '127.0.0.1', 9000)
+    logging.info('server started at http://127.0.0.1:9000...')
+    return srv
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(init(loop))
+loop.run_forever()
