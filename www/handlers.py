19bacd92896d24c0b02653b8f38304b2387a938d
badxu <35223644@qq.com>
2018-09-01 11:55:19 +0800

    day-09 by mac

--- www/handlers.py
+++ www/handlers.py
@@ -8,6 +8,7 @@ from models import User, Comment, Blog, next_id
 def index(request):
     summary = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
     blogs = [
+        Blog(id='5', name='Badxu',summary=summary,created_at=time.time()-120),
         Blog(id='1', name='Test Blog', summary=summary, created_at=time.time()-120),
         Blog(id='2', name='Something New', summary=summary, created_at=time.time()-3600),
         Blog(id='3', name='Learn Swift', summary=summary, created_at=time.time()-7200)
@@ -16,3 +17,11 @@ def index(request):
         '__template__': 'blogs.html',
         'blogs': blogs
     }
+
+
+@get('/api/users')
+async def api_get_users():
+    users = await User.findAll(orderBy='created_at desc')
+    for u in users:
+        u.passwd = '******'
+    return dict(users=users)
